902c352b2e1979ae3d15d3172fda2ce2c7535b54
Alexander Juda <alexander.juda@zapatacomputing.com>
2023-02-22 19:16:34 +0100

    Update running instructions

--- examples/orquestra/hydrogen_demo/run.py
+++ examples/orquestra/hydrogen_demo/run.py
@@ -1,7 +1,11 @@
 """
-To run this script:
-1. Go to the repo root
-2. Run ``python -m examples.orquestra.hydrogen_demo.run``. This ensures that relative
+Prerequisites:
+1. Log in to the remote Orquestra cluster with ``orq login -s https://prod-d.orquestra.io``
+
+Running:
+1. Comment/uncomment "in_process"/"prod-d" lines to select the runtime to use.
+2. ``cd`` to the repo root.
+3. Run ``python -m examples.orquestra.hydrogen_demo.run``. This ensures that relative
    imports work correctly.
 """
 import time
