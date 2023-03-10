2f55a2f1679bc48f66624b2d2446724ffc6a90ab
Max Radin <radin.max@gmail.com>
2023-03-10 00:02:03 -0500

    Adding basic gsc test

--- tests/benchq/resource_estimation/test_graph_compilation.py
+++ tests/benchq/resource_estimation/test_graph_compilation.py
@@ -14,7 +14,7 @@ def test_get_resource_estimations_for_program_gives_correct_n_measurement_steps(
         physical_gate_time_in_seconds=1e-6,
     )
     error_budget = 1e-3
-    circuit = Circuit([H(0), RZ(np.pi/4)(0), CNOT(0, 1)])
+    circuit = Circuit([H(0), RZ(np.pi / 4)(0), CNOT(0, 1)])
     quantum_program = QuantumProgram(
         subroutines=[circuit], steps=1, calculate_subroutine_sequence=lambda x: [0]
     )
@@ -24,4 +24,12 @@ def test_get_resource_estimations_for_program_gives_correct_n_measurement_steps(
         architecture_model,
         plot=True,
     )
+    
     assert gsc_resource_estimates["n_measurement_steps"] == 3
+    assert gsc_resource_estimates["n_nodes"] == 3
+    assert gsc_resource_estimates["max_graph_degree"] == 2
+
+    # Note that error_budget is a bound for the sum of the gate synthesis error and
+    # logical error. Therefore the expression below is a loose upper bound for the
+    # logical error rate.
+    assert gsc_resource_estimates["logical_error_rate"] < error_budget
