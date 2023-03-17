f60e85c9abfab511aef2b925337bb06ecebb7512
Max Radin <radin.max@gmail.com>
2023-03-17 13:04:11 -0400

    Make PySCF optional (#18)
    * Making pyscf optional install
    
    * Adding pytest dependency because of openfermion
    
    * Fixing hydrogen chain example
    
    * Bugfix in advanced estimates
    
    * Fixing imports in tests
    
    * Requiring AVAS parameters for meanfield object generation
    
    * Refactoring install extras

--- src/benchq/problem_ingestion/__init__.py
+++ src/benchq/problem_ingestion/__init__.py
@@ -5,10 +5,4 @@ from .hamiltonian_generation import (
     generate_fermi_hubbard_jw_qubit_hamiltonian,
     generate_jw_qubit_hamiltonian_from_mol_data,
 )
-from .molecule_instance_generation import (
-    CYCLIC_OZONE_MOLECULE,
-    WATER_MOLECULE,
-    ChemistryApplicationInstance,
-    generate_hydrogen_chain_instance,
-)
 from .vlasov import get_vlasov_hamiltonian
