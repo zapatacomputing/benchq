5a3f3869f232929a00d9e2ec71001354bec9f8e4
mstechly <michal.stechly@gmail.com>
2023-03-20 19:04:55 -0400

    style: ignore mypy errors

--- src/benchq/compilation/transpilation.py
+++ src/benchq/compilation/transpilation.py
@@ -58,9 +58,9 @@ class DecomposeStandardRZ(DecompositionRule[GateOperation]):
             -np.pi / 2,
             -np.pi,
         ]
-        theta = float(operation.params[0]) if len(operation.params) > 0 else None
+        theta = float(operation.params[0]) if len(operation.params) > 0 else None  # type: ignore # noqa: E501
         return bool(
-            (operation.gate.name in ["RZ"] and np.isclose(theta, special_angles).any())
+            (operation.gate.name in ["RZ"] and np.isclose(theta, special_angles).any())  # type: ignore # noqa: E501
         )
 
     def production(self, operation: GateOperation) -> Iterable[GateOperation]:
