7b2f3c03356fbb7e109fd93c9058b60c93d20569
ScottPJones <scottjones@alum.mit.edu>
2023-03-29 13:25:25 -0400

    Setup Julia only once

--- src/benchq/compilation/jabalizer_utils.py
+++ src/benchq/compilation/jabalizer_utils.py
@@ -1,15 +1,9 @@
 ################################################################################
 # © Copyright 2022-2023 Zapata Computing Inc.
 ################################################################################
-
-from juliacall import Main as jl
-
-import os
-import pathlib
-
 import networkx as nx
+from . import jl
 
 def get_algorithmic_graph(circuit):
-    jl.include(os.path.join(pathlib.Path(__file__).parent.resolve(), "jabalizer_wrapper.jl"))
     jl.run_jabalizer(circuit)
     return nx.read_adjlist("adjacency_list.nxl")
