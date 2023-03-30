3173489dd2b1cf351ca7b95a90080a101964ada5
ScottPJones <scottjones@alum.mit.edu>
2023-03-30 09:58:28 -0400

    Update to add stim

--- setup_julia.py
+++ setup_julia.py
@@ -2,5 +2,5 @@ import juliapkg
 
 if __name__ == "__main__":
     juliapkg.require_julia("1.8")
-    juliapkg.add("Jabalizer", "5ba14d91-d028-496b-b148-c0fbc366f709", version="0.4")
+    juliapkg.add("Jabalizer", "5ba14d91-d028-496b-b148-c0fbc366f709", version=">=0.4.4")
     juliapkg.resolve()
--- src/benchq/compilation/__init__.py
+++ src/benchq/compilation/__init__.py
@@ -6,10 +6,10 @@ import pathlib
 
 from juliacall import Main as jl
 
-jl.include(
-    os.path.join(pathlib.Path(__file__).parent.resolve(), "jabalizer_wrapper.jl")
-)
-
 from .jabalizer_utils import get_algorithmic_graph
 from .pyliqtr_compilation import pyliqtr_transpile_to_clifford_t
 from .transpilation import simplify_rotations
+
+jl.include(
+    os.path.join(pathlib.Path(__file__).parent.resolve(), "jabalizer_wrapper.jl")
+)
