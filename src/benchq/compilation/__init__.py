59c2eaee498be4f531447ace09f52cd30f8b0bf4
Scott P. Jones <ScottPJones@users.noreply.github.com>
2023-03-30 12:05:11 -0400

    Changes to allow directly calling Jabalizer from Python (#23)
    * Changes for direct call into Julia
    
    * Setup Julia only once
    
    * Updated to fix formatting
    
    * Update to add stim
    
    * Update Jabalizer versions
    
    * Update for coverage tests

--- src/benchq/compilation/__init__.py
+++ src/benchq/compilation/__init__.py
@@ -1,6 +1,15 @@
 ################################################################################
 # © Copyright 2022-2023 Zapata Computing Inc.
 ################################################################################
-from .jabalizer_utils import get_algorithmic_graph, load_algorithmic_graph
+import os
+import pathlib
+
+from juliacall import Main as jl
+
+from .jabalizer_utils import get_algorithmic_graph
 from .pyliqtr_compilation import pyliqtr_transpile_to_clifford_t
 from .transpilation import simplify_rotations
+
+jl.include(
+    os.path.join(pathlib.Path(__file__).parent.resolve(), "jabalizer_wrapper.jl")
+)
--- src/benchq/compilation/jabalizer_wrapper.jl
+++ src/benchq/compilation/jabalizer_wrapper.jl
@@ -3,21 +3,17 @@ using JSON
 
 ### ICM PART STARTS HERE
 
-
-function icm_input(filename)
-    raw_circuit = JSON.parsefile(filename)
-
-    n_qubits = pop!(raw_circuit)
-    circuit = Vector{Tuple{String,Vector{String}}}()
-
-    for gate in raw_circuit
-        gate_to_add = (gate[1], Vector{String}(gate[2]))
-        append!(circuit, [gate_to_add])
-    end
-    n_qubits, circuit
-end
-
 icm_compile(circuit, n_qubits) = Jabalizer.compile(circuit, n_qubits, ["T", "RX", "RY", "RZ"])
+
+### JABALIZER PART STARTS HERE
+
+const debug_flag = Ref(true)
+
+function out_cnt(opcnt, prevcnt, tn, pt, t0)
+    print("  Ops: $opcnt ($(opcnt-prevcnt)), ")
+    print("elapsed: $(round((tn-t0)/60_000_000_000, digits=2)) min (")
+    println(round((tn - pt) / 1_000_000_000, digits=2), " s)")
+end
 
 function icm_output_circuit(filename, circuit, data_qubits_map)
     dict = Dict()
@@ -28,16 +24,6 @@ function icm_output_circuit(filename, circuit, data_qubits_map)
     open(filename, "w") do f
         write(f, json_string)
     end
-end
-
-### JABALIZER PART STARTS HERE
-
-const debug_flag = Ref(true)
-
-function out_cnt(opcnt, prevcnt, tn, pt, t0)
-    print("  Ops: $opcnt ($(opcnt-prevcnt)), ")
-    print("elapsed: $(round((tn-t0)/60_000_000_000, digits=2)) min (")
-    println(round((tn - pt) / 1_000_000_000, digits=2), " s)")
 end
 
 function map_qubits(num_qubits, icm_output)
@@ -55,22 +41,8 @@ function map_qubits(num_qubits, icm_output)
     num_qubits, qubit_map
 end
 
-# This code is based on execute_cirq_circuit from Jabalizer
-
 function prepare(num_qubits, qubit_map, icm_output)
-    gate_map = Dict("I" => Jabalizer.Id,
-        "H" => Jabalizer.H,
-        "X" => Jabalizer.X,
-        "Y" => Jabalizer.Y,
-        "Z" => Jabalizer.Z,
-        "CNOT" => Jabalizer.CNOT,
-        "SWAP" => Jabalizer.SWAP,
-        "S" => Jabalizer.P,
-        "CZ" => Jabalizer.CZ)
-
-    print("zero_state: ")
-    @time state = zero_state(num_qubits)
-
+    state = zero_state(num_qubits)
     chkcnt = prevbits = prevop = opcnt = 0
     pt = t0 = time_ns()
     # loops over all operations in the circuit and applies them to the state
@@ -78,9 +50,9 @@ function prepare(num_qubits, qubit_map, icm_output)
         opcnt += 1
         len = length(op_qubits)
         if len == 1
-            (gate_map[op_name](qubit_map[op_qubits[1]]))(state)
+            (Jabalizer.gate_map[op_name](qubit_map[op_qubits[1]]))(state)
         elseif len == 2
-            (gate_map[op_name](qubit_map[op_qubits[1]], qubit_map[op_qubits[2]]))(state)
+            (Jabalizer.gate_map[op_name](qubit_map[op_qubits[1]], qubit_map[op_qubits[2]]))(state)
         else
             error("Too many arguments to $op_name: $len")
         end
@@ -97,25 +69,34 @@ function prepare(num_qubits, qubit_map, icm_output)
     state
 end
 
-print("Input ICM circuit\n\t")
-@time (n_qubits, circuit) = icm_input("icm_input_circuit.json")
+function run_jabalizer(circuit)
+    # Convert to Julia values, and (as a temp hack) rename T_Dagger to T (until implemented)
+    n_qubits = Jabalizer.pyconvert(Int, circuit.n_qubits)
+    icm_input = Jabalizer.ICMGate[]
+    for op in circuit.operations
+        name = Jabalizer.pyconvert(String, op.gate.name)
+        # TODO: THIS IS NOT CORRECT! TEMPORARY HACK!
+        name == "T_Dagger" && (name = "T")
+        indices = [string(Jabalizer.pyconvert(Int, qubit)) for qubit in op.qubit_indices]
+        push!(icm_input, (name, indices))
+    end
 
-print("ICM compilation: qubits=$n_qubits, gates=$(length(circuit))\n\t")
-@time (icm_output, data_qubits_map) = icm_compile(circuit, n_qubits)
+    print("ICM compilation: qubits=$n_qubits, gates=$(length(icm_input))\n\t")
+    @time (icm_output, data_qubits_map) = icm_compile(icm_input, n_qubits)
 
-print("Output ICM circuit\n\t")
-@time icm_output_circuit("icm_output.json", icm_output, data_qubits_map)
+    print("Output ICM circuit\n\t")
+    @time icm_output_circuit("icm_output.json", icm_output, data_qubits_map)
 
-print("Get total number of qubits\n\t")
-@time (n_qubits, qubit_map) = map_qubits(n_qubits, icm_output)
+    (n_qubits, qubit_map) = map_qubits(n_qubits, icm_output)
 
-print("Jabalizer state preparation: qubits=$n_qubits, gates=$(length(icm_output))\n\t")
-@time state = prepare(n_qubits, qubit_map, icm_output)
+    print("Jabalizer state preparation: qubits=$n_qubits, gates=$(length(icm_output))\n\t")
+    @time state = prepare(n_qubits, qubit_map, icm_output)
 
-print("Jabalizer graph generation: $n_qubits\n\t")
-@time (svec, op_seq) = graph_as_stabilizer_vector(state)
+    print("Jabalizer graph generation: $n_qubits\n\t")
+    @time (svec, op_seq) = graph_as_stabilizer_vector(state)
 
-print("Write Adjacency List: ")
-@time write_adjlist(svec, "adjacency_list.nxl")
+    print("Write Adjacency List: ")
+    @time write_adjlist(svec, "adjacency_list.nxl")
 
-println("Jabalizer finished")
+    println("Jabalizer finished")
+end
