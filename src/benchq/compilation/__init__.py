4a09f5713692509d229de70f08783b832c0873d6
AthenaCaesura <athena.caesura@zapatacomputing.com>
2023-03-30 13:24:14 -0400

    fix: merge conflicts

--- src/benchq/compilation/__init__.py
+++ src/benchq/compilation/__init__.py
@@ -7,7 +7,7 @@ import pathlib
 from juliacall import Main as jl
 
 from .gate_stitching import get_algorithmic_graph_from_gate_stitching
-from .jabalizer_utils import get_algorithmic_graph, load_algorithmic_graph
+from .jabalizer_utils import get_algorithmic_graph
 from .pyliqtr_compilation import pyliqtr_transpile_to_clifford_t
 from .transpilation import simplify_rotations
 
