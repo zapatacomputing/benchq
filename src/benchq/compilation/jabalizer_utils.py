97121c3fe527dd01457c5b85aabfbd7dd6c61fd5
ScottPJones <scottjones@alum.mit.edu>
2023-04-01 16:45:08 -0400

    Create adjacency list and return with icm output

--- src/benchq/compilation/jabalizer_utils.py
+++ src/benchq/compilation/jabalizer_utils.py
@@ -6,6 +6,22 @@ import networkx as nx
 from . import jl
 
 
+def create_adjlist_from_stabilizers(svec):
+    G = nx.Graph()
+    siz = len(svec)
+    for i in range(siz):
+        z = svec[i].Z
+        for j in range(i + 1, siz):
+            if z[j]:
+                G.add_edge(i, j)
+    return G
+
+
 def get_algorithmic_graph(circuit):
-    jl.run_jabalizer(circuit)
-    return nx.read_adjlist("adjacency_list.nxl")
+    svec, op_seq, icm_output, data_qubits_map = jl.run_jabalizer(circuit)
+    return create_adjlist_from_stabilizers(svec)
+
+
+def get_algorithmic_graph_and_icm_output(circuit):
+    svec, op_seq, icm_output, data_qubits_map = jl.run_jabalizer(circuit)
+    return create_adjlist_from_stabilizers(svec), op_seq, icm_output, data_qubits_map
