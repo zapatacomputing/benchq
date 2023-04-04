576fa0792cb1643f9d950d3041e9ac0d4c7339ae
Konrad Jałowiecki <dexter2206@gmail.com>
2023-04-04 20:37:26 +0200

    Update reexports from __init__ file

--- src/benchq/resource_estimation/v2/__init__.py
+++ src/benchq/resource_estimation/v2/__init__.py
@@ -1,11 +1,18 @@
 """Initialization file for benchq.resource_estimation.v2 subpackage."""
 from .estimators import GraphResourceEstimator
 from .pipelines import run_resource_estimation_pipeline
-from .transformers import simplify_only, synthesize_clifford_t
+from .transformers import (
+    synthesize_clifford_t,
+    simplify_rotations,
+    create_big_graph_from_subcircuits,
+    create_graphs_for_subcircuits,
+)
 
 __all__ = [
     "run_resource_estimation_pipeline",
     "synthesize_clifford_t",
-    "simplify_only",
+    "simplify_rotations",
+    "create_big_graph_from_subcircuits",
+    "create_graphs_for_subcircuits",
     "GraphResourceEstimator",
 ]
