e62e5060bd557d2f324a70017dc2a49c6b02291d
mstechly <michal.stechly@gmail.com>
2023-04-10 15:24:13 -0400

    Decoders improvements.

--- src/benchq/data_structures/decoder.py
+++ src/benchq/data_structures/decoder.py
@@ -6,75 +6,115 @@ from csv import reader
 
 
 class DecoderModel:
+    """Class representing decoder model
+
+    All the fields of the class represent some properties of the decoder model.
+    """
+
     def __init__(
         self,
         power_d_26: float,
         power_ranks: np.ndarray,
-        power_sqmat: np.ndarray,
+        power_sqmat_inv: np.ndarray,
         area_d_26: float,
         area_ranks: np.ndarray,
-        area_sqmat: np.ndarray,
+        area_sqmat_inv: np.ndarray,
         delay_d_26: float,
         delay_ranks: np.ndarray,
-        delay_sqmat: np.ndarray,
+        delay_sqmat_inv: np.ndarray,
         L2=10,
     ):
         self.power_d_26 = power_d_26
         self.power_ranks = power_ranks
-        self.power_sqmat = power_sqmat
+        self.power_sqmat_inv = power_sqmat_inv
         self.area_d_26 = area_d_26
         self.area_ranks = area_ranks
-        self.area_sqmat = area_sqmat
+        self.area_sqmat_inv = area_sqmat_inv
         self.delay_d_26 = delay_d_26
         self.delay_ranks = delay_ranks
-        self.delay_sqmat = delay_sqmat
+        self.delay_sqmat_inv = delay_sqmat_inv
         self.L2 = L2
 
-    def power(self, distance: int):
+    def power(self, distance: int) -> float:
+        """Calculates the power (in W) that it will take to decode the code
+        of given distance.
+
+        Args:
+            distance: surface code distance.
+        """
         return _get_estimate(
-            distance, self.L2, self.power_d_26, self.power_ranks, self.power_sqmat
+            distance, self.L2, self.power_d_26, self.power_ranks, self.power_sqmat_inv
         )
 
-    def area(self, distance: int):
+    def area(self, distance: int) -> float:
+        """Calculates the area (in m^2) that it will take to have a decoder
+        which allows to decode code of given distance.
+
+        Args:
+            distance: surface code distance.
+        """
         return _get_estimate(
-            distance, self.L2, self.area_d_26, self.area_ranks, self.area_sqmat
+            distance, self.L2, self.area_d_26, self.area_ranks, self.area_sqmat_inv
         )
 
-    def delay(self, distance: int):
+    def delay(self, distance: int) -> float:
+        """Calculates the delay (in s) it will take to decode the code
+        of given distance.
+
+        Args:
+            distance: surface code distance.
+        """
         return _get_estimate(
-            distance, self.L2, self.delay_d_26, self.delay_ranks, self.delay_sqmat
+            distance, self.L2, self.delay_d_26, self.delay_ranks, self.delay_sqmat_inv
         )
 
     @classmethod
-    def from_csv(cls, file_name):
-        data = np.genfromtxt(file_name, delimiter=",")
+    def from_csv(cls, file_path):
+        """Creates DecoderModel object based on a csv file.
+        The csv file should contain 3 rows, for data about power, area and delay.
+        Each row should contain 8 values:
+        - first corresponds to "d_26"
+        - next three are "ranks"
+        - last four are "sqmat_inv"
+
+        Args:
+            file_path: path of the file containing decoder data.
+        """
+
+        data = np.genfromtxt(file_path, delimiter=",")
+        if data.shape != (3, 8):
+            raise ValueError(
+                "Data for creating DecoderModel don't comply to the required format."
+            )
         power_d_26 = data[0][0]
         power_ranks = data[0][1:4]
-        power_sqmat = data[0][4:8]
+        power_sqmat_inv = data[0][4:8]
         area_d_26 = data[1][0]
         area_ranks = data[1][1:4]
-        area_sqmat = data[1][4:8]
+        area_sqmat_inv = data[1][4:8]
         delay_d_26 = data[2][0]
         delay_ranks = data[2][1:4]
-        delay_sqmat = data[2][4:8]
+        delay_sqmat_inv = data[2][4:8]
 
         return cls(
             power_d_26,
             power_ranks,
-            power_sqmat,
+            power_sqmat_inv,
             area_d_26,
             area_ranks,
-            area_sqmat,
+            area_sqmat_inv,
             delay_d_26,
             delay_ranks,
-            delay_sqmat,
+            delay_sqmat_inv,
         )
 
 
 def _get_estimate(
-    x: float, L2: float, d_26: float, ranks: np.ndarray, sqmat: np.ndarray
+    x: float, L2: float, d_26: float, ranks: np.ndarray, sqmat_inv: np.ndarray
 ) -> float:
-    return _d_26_term(x, d_26, L2) + _ranks_term(x, ranks) + _sqmat_term(x, sqmat)
+    return (
+        _d_26_term(x, d_26, L2) + _ranks_term(x, ranks) + _sqmat_inv_term(x, sqmat_inv)
+    )
 
 
 def _polynomial(x: float, coeffs: np.ndarray) -> float:
@@ -90,6 +130,6 @@ def _ranks_term(x: float, ranks: np.ndarray):
     return (2 * (2 * x * (x - 1) + 1)) * ranks_value
 
 
-def _sqmat_term(x: float, sqmat: np.ndarray):
-    sqmat_value = _polynomial(x**2, sqmat)
-    return sqmat_value / 1000
+def _sqmat_inv_term(x: float, sqmat_inv: np.ndarray):
+    sqmat_inv_value = _polynomial(x**2, sqmat_inv)
+    return sqmat_inv_value / 1000
--- tests/benchq/data_structures/test_decoder.py
+++ tests/benchq/data_structures/test_decoder.py
@@ -1,25 +1,31 @@
+import pytest
 from benchq.data_structures import DecoderModel
 import os
 import numpy as np
 
 
 class TestDecoders:
-    def file_path(self):
+    def file_path(self, file_name="decoder_test_data.csv"):
         curent_dir = os.path.dirname(os.path.abspath(__file__))
-        file_name = "decoders_data_test.csv"
+
         return os.path.join(curent_dir, file_name)
 
     def test_create_decoders_from_csv(self):
         decoder = DecoderModel.from_csv(self.file_path())
         assert decoder.power_d_26 == 0.3
         assert len(decoder.power_ranks) == 3
-        assert len(decoder.power_sqmat) == 4
+        assert len(decoder.power_sqmat_inv) == 4
         assert decoder.area_d_26 == 15000
         assert len(decoder.area_ranks) == 3
-        assert len(decoder.area_sqmat) == 4
+        assert len(decoder.area_sqmat_inv) == 4
         assert decoder.delay_d_26 == 15000
         assert len(decoder.delay_ranks) == 3
-        assert len(decoder.delay_sqmat) == 4
+        assert len(decoder.delay_sqmat_inv) == 4
+
+    def test_create_decoders_from_csv_throws_exception_for_invalid_data(self):
+        file_name = "decoder_test_data_corrupted.csv"
+        with pytest.raises(ValueError):
+            _ = DecoderModel.from_csv(self.file_path(file_name))
 
     def test_properties_have_same_formulas(self):
         d_26 = 1
@@ -46,3 +52,16 @@ class TestDecoders:
         assert (
             decoder.power(distance) == decoder.area(distance) == decoder.delay(distance)
         )
+
+    def test_decoder_estimates_didnt_change(self):
+        decoder = DecoderModel.from_csv(self.file_path())
+        distance = 10
+        # These tests compare to hardcoded values for a particular set of params
+        # at the time of writing these.
+        # Their goal is to make sure that we have not accidentally changed the logic.
+        # However, it might be that the logic was faulty from the beginning and
+        # fixing the logic will make this test fail.
+        # In such case it should be edited.
+        assert decoder.power(distance) == 30966.8702
+        assert decoder.area(distance) == 1126336933.5
+        assert decoder.delay(distance) == 323571881.5
