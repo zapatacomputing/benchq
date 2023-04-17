f54c0a4d7987eafe423abf7b8495e574d426ff80
mstechly <michal.stechly@gmail.com>
2023-04-17 14:21:49 -0400

    Minor fixes in example.

--- examples/packages_comparison.py
+++ examples/packages_comparison.py
@@ -7,17 +7,12 @@ from benchq import BasicArchitectureModel
 from benchq.algorithms.time_evolution import get_qsp_time_evolution_program
 from benchq.data_structures.quantum_program import get_program_from_circuit
 from benchq.problem_ingestion import get_vlasov_hamiltonian
-from benchq.problem_ingestion.hamiltonian_generation import (
-    fast_load_qubit_op,
-    generate_1d_heisenberg_hamiltonian,
-)
 from benchq.resource_estimation.azure import AzureResourceEstimator
 from benchq.resource_estimation.graph import (
     GraphResourceEstimator,
     create_big_graph_from_subcircuits,
     run_resource_estimation_pipeline,
     simplify_rotations,
-    synthesize_clifford_t,
 )
 from benchq.timing import measure_time
 
@@ -38,6 +33,8 @@ def main():
     )  # Allocate half the error budget to trotter precision
 
     error_budget = {
+        "qsp_required_precision": qsp_required_precision,
+        "tolerable_circuit_error_rate": tolerable_logical_error_rate,
         "total_error": 1e-2,
         "synthesis_error_rate": 0.5,
         "ec_error_rate": 0.5,
@@ -84,15 +81,15 @@ def main():
     pprint(gsc_resource_estimates)
 
     with measure_time() as t_info:
-        gsc_resource_estimates = run_resource_estimation_pipeline(
+        azure_resource_estimates = run_resource_estimation_pipeline(
             program,
             error_budget,
             estimator=AzureResourceEstimator(architecture_model),
             transformers=[],
         )
 
-    print("Resource estimation time with synthesis:", t_info.total)
-    pprint(gsc_resource_estimates)
+    print("Resource estimation time with Azure:", t_info.total)
+    pprint(azure_resource_estimates)
 
 
 if __name__ == "__main__":
