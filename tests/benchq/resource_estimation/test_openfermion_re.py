a6aafdcb00b3a824bf31aeee9d8da1a757eb53c4
Max Radin <radin.max@gmail.com>
2023-04-27 15:40:14 -0400

    Fixing style

--- tests/benchq/resource_estimation/test_openfermion_re.py
+++ tests/benchq/resource_estimation/test_openfermion_re.py
@@ -28,13 +28,12 @@ def test_physical_qubit_count_is_larger_than_number_of_spin_orbitals(
         > 2 * mean_field_object._eri.shape[0]
     )
 
+
 def test_invalid_eri_raise_exception():
     instance = generate_hydrogen_chain_instance(8)
-    instance.avas_atomic_orbitals =  ["H 1s", "H 2s"]
-    instance.avas_minao =  "sto-3g"
+    instance.avas_atomic_orbitals = ["H 1s", "H 2s"]
+    instance.avas_minao = "sto-3g"
     mean_field_object = instance.get_active_space_meanfield_object()
     mean_field_object._eri[0, 1, 2, 3] += 0.1
     with pytest.raises(ValueError):
-        get_qpe_resource_estimates_from_mean_field_object(
-            mean_field_object
-        )
+        get_qpe_resource_estimates_from_mean_field_object(mean_field_object)
