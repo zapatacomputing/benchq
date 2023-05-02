5951da7f2e0ff3ca9e24f3498c7f920b4283aac9
Max Radin <radin.max@gmail.com>
2023-05-02 11:40:35 -0400

    OpenFermion estimates without AVAS (#59)
    * Adding openfermion resource estimation test
    
    * Adding check for ERI permutation symmetry
    
    * Removing duplicate openfermion file
    
    * Adding check for (ij|kl) == (kl|ij) symmetry
    
    * Fixing style

--- src/benchq/resource_estimation/__init__.py
+++ src/benchq/resource_estimation/__init__.py
@@ -1,4 +1,4 @@
 ################################################################################
 # © Copyright 2022 Zapata Computing Inc.
 ################################################################################
-from .openfermion import get_qpe_resource_estimates_from_mean_field_object
+from .openfermion_re import get_qpe_resource_estimates_from_mean_field_object
