58621eb5e28b02332ab9d818e42b92a5ea1911b5
Konrad Jałowiecki <dexter2206@gmail.com>
2023-05-15 11:33:51 +0200

    Create ExtrapolatedGraphData structure

--- src/benchq/data_structures/resource_info.py
+++ src/benchq/data_structures/resource_info.py
@@ -3,7 +3,7 @@
 ################################################################################
 """Data structures describing estimated resources and related info."""
 from dataclasses import dataclass, field
-from typing import Generic, Optional, TypeVar
+from typing import Generic, List, Optional, TypeVar
 
 TExtra = TypeVar("TExtra")
 
@@ -14,17 +14,6 @@ class DecoderInfo:
     power: float
     area: float
     max_decodable_distance: int = field(repr=False)
-
-
-@dataclass
-class GraphData:
-    """Contains minimal set of data to get a resource estimate for a graph."""
-
-    max_graph_degree: int
-    n_nodes: int
-    n_t_gates: int
-    n_rotation_gates: int
-    n_measurement_steps: int
 
 
 @dataclass
@@ -40,4 +29,23 @@ class ResourceInfo(Generic[TExtra]):
     extra: TExtra
 
 
+@dataclass
+class GraphData:
+    """Contains minimal set of data to get a resource estimate for a graph."""
+
+    max_graph_degree: int
+    n_nodes: int
+    n_t_gates: int
+    n_rotation_gates: int
+    n_measurement_steps: int
+
+
 GraphCompilationResourceInfo = ResourceInfo[GraphData]
+
+
+@dataclass
+class ExtrapolatedGraphData(GraphData):
+    n_logical_qubits_r_squared: float
+    n_measurement_steps_r_squared: float
+    data_used_to_extrapolate: List[GraphCompilationResourceInfo]
+    steps_to_extrapolate_to: int
