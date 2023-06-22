72aa3581ad5c077c137e12537e508ddce20a881c
Max Radin <radin.max@gmail.com>
2023-06-22 15:59:15 -0400

    Updating LD-GSEE docstrings

--- src/benchq/algorithms/ld_gsee.py
+++ src/benchq/algorithms/ld_gsee.py
@@ -6,10 +6,22 @@
 import numpy as np
 
 
-def _get_sigma(
+def get_sigma(
     alpha: float, energy_gap: float, lam: float, eta: float, DE: float
 ) -> float:
-    """Get the standard deviation of the Gaussian convolution function (Eq. 15 of arXiv:2209.06811v2)."""
+    """Get the standard deviation of the Gaussian convolution function (Eq. 15 of
+    arXiv:2209.06811v2).
+
+    Arguments:
+        alpha: The parameter alpha controlling the tradeoff between circuit repetitions
+            and circuit depth. Zero corresponds to the minimum circuit depth, while one
+            corresponds to the minimum number of circuit repetitions.
+        energy_gap: The energy gap of the Hamiltonian.
+        lam: The one-norm of the Hamiltonian.
+        eta: The square overlap of the initial state with the ground state.
+        DE: The desired ground state energy accuracy.
+
+    Returns: The standard deviation of the Gaussian convolution function."""
     epsilon = DE / lam
     delta = epsilon**alpha * energy_gap ** (1 - alpha)
     return min(
@@ -18,13 +30,29 @@ def _get_sigma(
     )
 
 
-def _get_epsilon_1(epsilon: float, eta: float, sigma: float) -> float:
-    """Get the error with respect to the convolution function (Eq. 28 of arXiv:2209.06811v2)."""
+def get_epsilon_1(epsilon: float, eta: float, sigma: float) -> float:
+    """Get the error with respect to the convolution function (Eq. 28 of
+        arXiv:2209.06811v2).
+
+    Arguments:
+        epsilon: The error tolerance.
+        eta: The error tolerance for the convolution function.
+        sigma: The standard deviation of the Gaussian convolution function.
+
+    Returns: The error with respect to the convolution function."""
+
     return 0.1 * epsilon * eta / (np.sqrt(2 * np.pi) * sigma**3)
 
 
 def get_ldgsee_num_iterations(sigma: float, epsilon_1: float) -> float:
-    """Get the number of iterations for the FF-LD-GSEE algorithm (Eq. 34 of arXiv:2209.06811v2)."""
+    """Get the number of iterations for the FF-LD-GSEE algorithm (Eq. 34 of
+    arXiv:2209.06811v2).
+
+    Arguments:
+        sigma: The standard deviation of the Gaussian convolution function.
+        epsilon_1: The error with respect to the convolution function.
+
+    Returns: The estimated number of iterations required by the FF-LD-GSEE algorithm."""
     return (
         np.pi ** (-1)
         * sigma ** (-1)
@@ -35,7 +63,16 @@ def get_ldgsee_num_iterations(sigma: float, epsilon_1: float) -> float:
 def get_ldgsee_num_circuit_repetitions(
     epsilon: float, sigma: float, epsilon_1: float, failure_probability: float
 ) -> float:
-    """Get the number of circuit repetitions for the FF-LD-GSEE algorithm."""
+    """Get the number of circuit repetitions for the FF-LD-GSEE algorithm.
+
+    Arguments:
+        epsilon: The error tolerance.
+        sigma: The standard deviation of the Gaussian convolution function.
+        epsilon_1: The error with respect to the convolution function.
+        failure_probability: The failure probability.
+
+    Returns: The estimated number of circuit repetitions required by the FF-LD-GSEE
+        algorithm.."""
 
     # number of grid points along the x-axis
     M = np.ceil(sigma / epsilon) + 1
