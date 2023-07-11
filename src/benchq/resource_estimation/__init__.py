e5b44420f3af5f7ab8c96d5594fc3b3d3c896c6c
Bhargav <bhargav19036@gmail.com>
2023-07-11 16:43:16 -0400

    ran style check commands

--- src/benchq/resource_estimation/__init__.py
+++ src/benchq/resource_estimation/__init__.py
@@ -1,5 +1,5 @@
 ################################################################################
 # © Copyright 2022 Zapata Computing Inc.
 ################################################################################
-from .openfermion_re import get_single_factorized_qpe_resource_estimate
 from .of_modified import cost_estimator
+from .openfermion_re import get_single_factorized_qpe_resource_estimate
