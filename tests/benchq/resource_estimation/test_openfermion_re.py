2191b56e67147a38479f51b67adc111adf36dc68
Bhargav <bhargav19036@gmail.com>
2023-07-10 21:44:26 -0400

    Test for checking monotonicity of run-time wrt SCC

--- tests/benchq/resource_estimation/test_openfermion_re.py
+++ tests/benchq/resource_estimation/test_openfermion_re.py
@@ -5,6 +5,7 @@ from benchq.problem_ingestion.molecule_instance_generation import (
     generate_hydrogen_chain_instance,
 )
 from benchq.resource_estimation import get_single_factorized_qpe_resource_estimate
+import datetime
 
 
 @pytest.mark.parametrize(
@@ -26,8 +27,45 @@ def test_physical_qubit_count_is_larger_than_number_of_spin_orbitals(
     qpe_resource_estimates = get_single_factorized_qpe_resource_estimate(
         h1, eri_full, 20
     )
+    print("Resource estimates:", qpe_resource_estimates.n_physical_qubits)
     assert (
         qpe_resource_estimates.n_physical_qubits > 2 * mean_field_object._eri.shape[0]
+    )
+
+
+@pytest.mark.parametrize(
+    "SCC_time_low,SCC_time_high",
+    [
+        (datetime.timedelta(seconds=0.000001), datetime.timedelta(seconds=0.000008)),
+        (datetime.timedelta(seconds=0.000004), datetime.timedelta(seconds=0.000009)),
+        (datetime.timedelta(seconds=0.000004), datetime.timedelta(seconds=0.000009)),
+        (datetime.timedelta(seconds=0.000005), datetime.timedelta(seconds=0.000010)),
+    ],
+)
+def test_monotonicity_of_duration_wrt_SCC_time(SCC_time_low, SCC_time_high):
+    instance = generate_hydrogen_chain_instance(8)
+    instance.avas_atomic_orbitals = ["H 1s", "H 2s"]
+    instance.avas_minao = "sto-3g"
+    mean_field_object = instance.get_active_space_meanfield_object()
+    h1, eri_full, _, _, _ = pyscf_to_cas(mean_field_object)
+
+    qpe_resource_estimates_low = get_single_factorized_qpe_resource_estimate(
+        h1, eri_full, 20, SCC_time_low
+    )
+
+    qpe_resource_estimates_high = get_single_factorized_qpe_resource_estimate(
+        h1, eri_full, 20, SCC_time_high
+    )
+
+    # assert  (qpe_resource_estimates_low_1.total_time_in_seconds == qpe_resource_estimates_low_1.total_time_in_seconds)
+    print(
+        "High, low: ",
+        qpe_resource_estimates_high.total_time_in_seconds,
+        qpe_resource_estimates_low.total_time_in_seconds,
+    )
+    assert (
+        qpe_resource_estimates_high.total_time_in_seconds
+        > qpe_resource_estimates_low.total_time_in_seconds
     )
 
 
