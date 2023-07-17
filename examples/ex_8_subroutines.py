5e5f3f1680f19c33df5293e0e07ca2c610d07ff4
Peter Johnson <peter.d.johnson22@gmail.com>
2023-07-16 23:37:12 -0400

    Refactoring to simplify the usage of child subroutines within a parent subroutine

--- examples/ex_8_subroutines.py
+++ examples/ex_8_subroutines.py
@@ -9,16 +9,16 @@ from benchq.algorithms.ld_gsee import (
 class LD_GSEE(SubroutineModel):
     def __init__(
         self,
-        name="ff_gsee",
+        task_name="ground_state_energy_estimation",
         requirements=None,
-        c_time_evolution_hadamard_test=SubroutineModel(
-            "c_time_evolution_hadamard_test"
-        ),
+        c_time_evolution=None,
     ):
         super().__init__(
-            name,
+            task_name,
             requirements,
-            c_time_evolution_hadamard_test=c_time_evolution_hadamard_test,
+            c_time_evolution=c_time_evolution
+            if c_time_evolution is not None
+            else SubroutineModel("c_time_evolution"),
         )
 
     def set_requirements(
@@ -27,54 +27,54 @@ class LD_GSEE(SubroutineModel):
         energy_gap,
         square_overlap,
         precision,
-        failure_probability,
+        failure_tolerance,
         hamiltonian,
     ):
         args = locals()
+        # Clean up the args dictionary before setting requirements
         args.pop("self")
+        args = {k: v for k, v in args.items() if not k.startswith("__")}
         super().set_requirements(**args)
 
-    def populate_subroutine_profile(self):
-        for i, (subroutine, count) in enumerate(self.subroutine_profile):
-            if subroutine.name == "c_time_evolution_hadamard_test":
-                # Compute number of samples
-                n_samples = get_ff_ld_gsee_num_circuit_repetitions(
-                    self.requirements["alpha"],
-                    self.requirements["energy_gap"],
-                    self.requirements["square_overlap"],
-                    self.requirements["precision"],
-                    self.requirements["failure_probability"],
-                )
-                self.subroutine_profile[i] = (subroutine, n_samples)
+    def populate_requirements_for_subroutines(self):
+        # Compute number of samples
+        n_samples = get_ff_ld_gsee_num_circuit_repetitions(
+            self.requirements["alpha"],
+            self.requirements["energy_gap"],
+            self.requirements["square_overlap"],
+            self.requirements["precision"],
+            self.requirements["failure_tolerance"],
+        )
+        self.c_time_evolution.number_of_times_called = n_samples
 
-                # Set controlled time evolution hadamard test requirements
-                hadamard_failure_rate = (
-                    self.requirements["failure_probability"] / n_samples
-                )
-                evolution_time = get_ff_ld_gsee_max_evolution_time(
-                    self.requirements["alpha"],
-                    self.requirements["energy_gap"],
-                    self.requirements["square_overlap"],
-                    self.requirements["precision"],
-                )
-                subroutine.set_requirements(
-                    evolution_time=evolution_time,
-                    hamiltonian=self.requirements["hamiltonian"],
-                    failure_rate=hadamard_failure_rate,
-                )
+        # Set controlled time evolution hadamard test requirements
+        hadamard_failure_rate = self.requirements["failure_tolerance"] / n_samples
+        evolution_time = get_ff_ld_gsee_max_evolution_time(
+            self.requirements["alpha"],
+            self.requirements["energy_gap"],
+            self.requirements["square_overlap"],
+            self.requirements["precision"],
+        )
+        self.c_time_evolution.set_requirements(
+            evolution_time=evolution_time,
+            hamiltonian=self.requirements["hamiltonian"],
+            failure_rate=hadamard_failure_rate,
+        )
 
 
-class CTimeEvolutionHadamardTest(SubroutineModel):
+class QSPTimeEvolution(SubroutineModel):
     def __init__(
         self,
-        name="c_time_evolution_hadamard_test",
+        task_name="c_time_evolution",
         requirements=None,
-        hamiltonian_block_encoding=SubroutineModel("hamiltonian_block_encoding"),
+        hamiltonian_block_encoding=None,
     ):
         super().__init__(
-            name,
+            task_name,
             requirements,
-            hamiltonian_block_encoding=hamiltonian_block_encoding,
+            hamiltonian_block_encoding=hamiltonian_block_encoding
+            if hamiltonian_block_encoding is not None
+            else SubroutineModel("hamiltonian_block_encoding"),
         )
 
     def set_requirements(
@@ -84,42 +84,43 @@ class CTimeEvolutionHadamardTest(SubroutineModel):
         failure_rate,
     ):
         args = locals()
+        # Clean up the args dictionary before setting requirements
         args.pop("self")
+        args = {k: v for k, v in args.items() if not k.startswith("__")}
         super().set_requirements(**args)
 
-    def populate_subroutine_profile(self):
-        for i, (subroutine, count) in enumerate(self.subroutine_profile):
-            if subroutine.name == "hamiltonian_block_encoding":
-                # Compute number of samples
-                n_block_encodings = 1 / self.requirements["failure_rate"]
-                self.subroutine_profile[i] = (subroutine, n_block_encodings)
-                n_block_encodings
-                be_failure_rate = self.requirements["failure_rate"] / n_block_encodings
-                subroutine.set_requirements(
-                    failure_rate=be_failure_rate,
-                )
+    def populate_requirements_for_subroutines(self):
+        # Compute number of samples
+        n_block_encodings = 1 / self.requirements["failure_rate"]
+        self.hamiltonian_block_encoding.number_of_times_called = n_block_encodings
+
+        be_failure_rate = self.requirements["failure_rate"] / n_block_encodings
+        self.hamiltonian_block_encoding.set_requirements(
+            failure_rate=be_failure_rate,
+        )
 
 
-# # cliff = Subroutine("clifford_gate")
-# # BE = DFBlockEncoding()
+# Create an instance of QSPTimeEvolution
+c_time_evolution = QSPTimeEvolution()
 
+# Create an instance of LD_GSEE with QSPTimeEvolution as a task
+ld_gsee = LD_GSEE(c_time_evolution=c_time_evolution)
 
-# # # Create a specific HadamardTest subroutine
-my_hadamard_test = CTimeEvolutionHadamardTest(
-    hamiltonian_block_encoding=SubroutineModel("hamiltonian_block_encoding"),
-)
-
-
-# Pass this specific HadamardTest to LD_GSEE
-my_ff_gsee = LD_GSEE(c_time_evolution_hadamard_test=my_hadamard_test)
-my_ff_gsee.set_requirements(
+# Set the requirements for LD_GSEE
+ld_gsee.set_requirements(
     alpha=0.5,
-    energy_gap=0.2,
-    square_overlap=0.1,
-    precision=0.01,
-    failure_probability=0.1,
-    hamiltonian=None,
+    energy_gap=0.3,
+    square_overlap=0.8,
+    precision=0.001,
+    failure_tolerance=0.1,
+    hamiltonian="H",
 )
-my_ff_gsee.run_profile()
-my_ff_gsee.print_profile()
-print(my_ff_gsee.count_subroutines())
+
+# Run the profile
+ld_gsee.run_profile()
+
+# Print the profile
+ld_gsee.print_profile()
+
+# Count the subroutines
+print(ld_gsee.count_subroutines())
--- src/benchq/data_structures/subroutine_model.py
+++ src/benchq/data_structures/subroutine_model.py
@@ -1,58 +1,131 @@
+# from typing import Optional
+
+
+# class SubroutineModel:
+#     def __init__(self, name: str, requirements: Optional[dict] = None, **kwargs):
+#         self.name = name
+#         self.requirements = requirements or {}
+#         # Initialize the subroutine_profile with the subroutines in the kwargs
+#         self.subroutine_profile = [
+#             (value, None)
+#             for attr, value in kwargs.items()
+#             if isinstance(value, SubroutineModel)
+#         ]
+
+#     def set_requirements(self, *args, **kwargs):
+#         """
+#         Sets the requirements using the passed arguments. The arguments passed to this method will be automatically
+#         converted to a dictionary and stored in the 'requirements' attribute.
+#         """
+
+#         # Check for positional arguments
+#         if args:
+#             raise TypeError(
+#                 "The set_requirements method expects keyword arguments of the form argument=value."
+#             )
+
+#         self.requirements = kwargs
+
+#     def populate_subroutine_profile(self):
+#         # Should decide how to treat this!
+#         pass
+#         # raise NotImplementedError("This method should be implemented in child classes.")
+
+#     def run_profile(self, requirements=None):
+#         self.populate_subroutine_profile()
+#         # if requirements is not None:
+#         #     self.requirements.update(requirements)
+#         for child, _ in self.subroutine_profile:
+#             child.run_profile()
+
+#     def print_profile(self, level=0):
+#         requirements_str = ", ".join(f"{k}: {v}" for k, v in self.requirements.items())
+#         print_str = f"Task: {self.name}, Requirements: {requirements_str}, Subroutine: {type(self)}"
+#         print("  " * level + print_str)
+#         for child, count in self.subroutine_profile:
+#             print("  " * level + f"Count: {count}")
+#             child.print_profile(level + 1)
+
+#     def count_subroutines(self):
+#         counts = {self.name: 1}
+#         for child, count in self.subroutine_profile:
+#             child_counts = child.count_subroutines()
+#             for name, child_count in child_counts.items():
+#                 if name in counts:
+#                     counts[name] += child_count * (count if count is not None else 1)
+#                 else:
+#                     counts[name] = child_count * (count if count is not None else 1)
+#         return counts
+
+
 from typing import Optional
 
 
 class SubroutineModel:
-    def __init__(self, name: str, requirements: Optional[dict] = None, **kwargs):
-        self.name = name
+    def __init__(self, task_name: str, requirements: Optional[dict] = None, **kwargs):
+        self.task_name = task_name
         self.requirements = requirements or {}
-        # Initialize the subroutine_profile with the subroutines in the kwargs
-        self.subroutine_profile = [
-            (value, None)
-            for attr, value in kwargs.items()
-            if isinstance(value, SubroutineModel)
-        ]
+        self.number_of_times_called = None
+        for attr, value in kwargs.items():
+            if isinstance(value, SubroutineModel):
+                setattr(self, attr, value)
 
     def set_requirements(self, *args, **kwargs):
-        """
-        Sets the requirements using the passed arguments. The arguments passed to this method will be automatically
-        converted to a dictionary and stored in the 'requirements' attribute.
-        """
-
-        # Check for positional arguments
         if args:
             raise TypeError(
                 "The set_requirements method expects keyword arguments of the form argument=value."
             )
-
         self.requirements = kwargs
 
-    def populate_subroutine_profile(self):
-        # Should decide how to treat this!
+    def populate_requirements_for_subroutines(self):
         pass
-        # raise NotImplementedError("This method should be implemented in child classes.")
 
     def run_profile(self, requirements=None):
-        self.populate_subroutine_profile()
-        # if requirements is not None:
-        #     self.requirements.update(requirements)
-        for child, _ in self.subroutine_profile:
-            child.run_profile()
+        self.populate_requirements_for_subroutines()
+        for attr in dir(self):
+            child = getattr(self, attr)
+            if isinstance(child, SubroutineModel):
+                child.run_profile()
 
     def print_profile(self, level=0):
-        requirements_str = ", ".join(f"{k}: {v}" for k, v in self.requirements.items())
-        print_str = f"SubroutineModel: {self.name}, Requirements: {requirements_str}"
-        print("  " * level + print_str)
-        for child, count in self.subroutine_profile:
-            print("  " * level + f"Count: {count}")
-            child.print_profile(level + 1)
+        # Format the requirements string to be more readable
+        requirements_str = "\n".join(
+            "  " * (level + 2) + f"{k}: {v}" for k, v in self.requirements.items()
+        )
+        print_str = (
+            f"{'  ' * level}Subroutine: {type(self).__name__} (Task: {self.task_name})"
+        )
+        print(print_str)
+        if (
+            requirements_str.strip()
+        ):  # only print "Requirements:" if there are requirements
+            print("  " * (level + 1) + "Requirements:")
+            print(requirements_str)
+        if self.number_of_times_called is not None:
+            print("  " * (level + 1) + f"Count: {self.number_of_times_called}")
+        # Recurse for child subroutines
+        for attr in dir(self):
+            child = getattr(self, attr)
+            if isinstance(child, SubroutineModel):
+                child.print_profile(level + 1)
 
     def count_subroutines(self):
-        counts = {self.name: 1}
-        for child, count in self.subroutine_profile:
-            child_counts = child.count_subroutines()
-            for name, child_count in child_counts.items():
-                if name in counts:
-                    counts[name] += child_count * (count if count is not None else 1)
-                else:
-                    counts[name] = child_count * (count if count is not None else 1)
+        counts = {self.task_name: 1}
+        for attr in dir(self):
+            child = getattr(self, attr)
+            if isinstance(child, SubroutineModel):
+                child_counts = child.count_subroutines()
+                for name, child_count in child_counts.items():
+                    if name in counts:
+                        counts[name] += child_count * (
+                            child.number_of_times_called
+                            if child.number_of_times_called is not None
+                            else 1
+                        )
+                    else:
+                        counts[name] = child_count * (
+                            child.number_of_times_called
+                            if child.number_of_times_called is not None
+                            else 1
+                        )
         return counts
