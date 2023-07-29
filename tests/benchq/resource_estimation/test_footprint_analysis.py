282763ad3b182e8eef89e4781e58439805d00b2a
Bhargav <bhargav19036@gmail.com>
2023-07-28 20:50:10 -0400

    updated tests

--- tests/benchq/resource_estimation/test_footprint_analysis.py
+++ tests/benchq/resource_estimation/test_footprint_analysis.py
@@ -5,7 +5,7 @@ from benchq.resource_estimation import cost_estimator
 
 
 @pytest.mark.parametrize(
-    "SCC_time_low,SCC_time_high",
+    "scc_time_low,scc_time_high",
     [
         (0.000001, 0.000002),
         (0.000003, 0.000004),
@@ -19,27 +19,30 @@ from benchq.resource_estimation import cost_estimator
         (0.000001, 0.000009),
     ],
 )
-def test_monotonicity_of_duration_wrtSurfaceCC_time(SCC_time_low, SCC_time_high):
+def test_monotonicity_of_duration_wrtSurfaceCC_time(scc_time_low, scc_time_high):
+    """
+    This tests if duration (run-time) increases as surface code cycle time increases
+    """
     num_logical_qubits = 12
     num_toffoli = 15
     physical_error_rate = 1.0e-4
     best_cost_low, best_params_low = cost_estimator(
         num_logical_qubits,
         num_toffoli=num_toffoli,
-        surface_code_cycle_time=SCC_time_low,
+        surface_code_cycle_time=scc_time_low,
         physical_error_rate=physical_error_rate,
     )
     best_cost_high, best_params_high = cost_estimator(
         num_logical_qubits,
         num_toffoli=num_toffoli,
-        surface_code_cycle_time=SCC_time_high,
+        surface_code_cycle_time=scc_time_high,
         physical_error_rate=physical_error_rate,
     )
     assert best_cost_high.duration > best_cost_low.duration
 
 
 @pytest.mark.parametrize(
-    "SCC_time_low,SCC_time_high",
+    "scc_time_low,scc_time_high",
     [
         (0.000001, 0.000002),
         (0.000003, 0.000004),
@@ -53,176 +56,200 @@ def test_monotonicity_of_duration_wrtSurfaceCC_time(SCC_time_low, SCC_time_high)
         (0.000001, 0.000009),
     ],
 )
-def test_linearity_wrtSurfaceCC_time(SCC_time_low, SCC_time_high):
+def test_linearity_wrtSurfaceCC_time(scc_time_low, scc_time_high):
+    """
+    This tests if duration (run-time) proportionately
+    increases wrt surface code cycle time
+    """
     num_logical_qubits = 12
-    num_T = 13
+    num_t = 13
     physical_error_rate = 1.0e-4
     best_cost_low, best_params_low = cost_estimator(
         num_logical_qubits,
-        num_T=num_T,
-        surface_code_cycle_time=SCC_time_low,
+        num_t=num_t,
+        surface_code_cycle_time=scc_time_low,
         physical_error_rate=physical_error_rate,
     )
     best_cost_high, best_params_high = cost_estimator(
         num_logical_qubits,
-        num_T=num_T,
-        surface_code_cycle_time=SCC_time_high,
+        num_t=num_t,
+        surface_code_cycle_time=scc_time_high,
         physical_error_rate=physical_error_rate,
     )
 
     numpy.testing.assert_almost_equal(
-        best_cost_high.duration / SCC_time_high, best_cost_low.duration / SCC_time_low
+        best_cost_high.duration / scc_time_high, best_cost_low.duration / scc_time_low
     )
 
 
 @pytest.mark.parametrize(
-    "num_toffoli,num_T",
+    "num_toffoli,num_t",
     [
         (20, 20),
         (40, 40),
         (20, 30),
     ],
 )
-def test_ratio_of_failure_prob(num_toffoli, num_T):
+def test_ratio_of_failure_prob(num_toffoli, num_t):
+    """
+    This ascertains if ratio of failure rate of magic state factory for a ckt
+    with only Toffoli gates and of a ckt with only T gates is 1:1, given the same
+    physical error rate and surface code cycle time.
+
+    """
     num_logical_qubits = 12
-    SCC_time = 0.000002
+    scc_time = 0.000002
     physical_error_rate = 1.0e-4
     best_cost_toffoli, best_params_toffoli = cost_estimator(
         num_logical_qubits=num_logical_qubits,
         num_toffoli=num_toffoli,
-        surface_code_cycle_time=SCC_time,
+        surface_code_cycle_time=scc_time,
         physical_error_rate=physical_error_rate,
     )
-    best_cost_T, best_params_T = cost_estimator(
+    best_cost_t, best_params_t = cost_estimator(
         num_logical_qubits=num_logical_qubits,
-        num_T=num_T,
-        surface_code_cycle_time=SCC_time,
+        num_t=num_t,
+        surface_code_cycle_time=scc_time,
         physical_error_rate=physical_error_rate,
     )
-    """
-    This ascertains if ratio of failure rate of Toffoli
-    and T Magic State factories is 2:1
-    """
 
     assert (
         best_params_toffoli.magic_state_factory.failure_rate
-        / best_params_T.magic_state_factory.failure_rate
-        == 2.0
+        / best_params_t.magic_state_factory.failure_rate
+        == 1.0
     )
 
 
 @pytest.mark.parametrize(
-    "num_toffoli,num_T",
+    "num_toffoli,num_t",
     [
         (20, 20),
         (40, 40),
     ],
 )
-def test_algorithm_failure_prob_calculation(num_toffoli, num_T):
+def test_calc_of_algorithm_failure_prob(num_toffoli, num_t):
+    """
+    X+Yf - 2*(X/2+Yf/2) = 0, where X+Yf and X+Yf/2 are
+    Algorithm Failure Prob for toffoli and t cases respectively,
+    where X -> data failure, Yf -> distillation failure,
+      Y=#toffoli gate=#t gates and f is 1 CCZ error.
+    If failure rate is calculated correctly, to ascertain
+    if Algorithm Failure Probability is calculated correctly
+    for both toffoli and t.
+    """
     num_logical_qubits = 12
-    SCC_time = 0.000002
+    scc_time = 0.000002
     physical_error_rate = 1.0e-4
     best_cost_toffoli, best_params_toffoli = cost_estimator(
         num_logical_qubits=num_logical_qubits,
         num_toffoli=num_toffoli,
-        surface_code_cycle_time=SCC_time,
+        surface_code_cycle_time=scc_time,
         physical_error_rate=physical_error_rate,
     )
-    best_cost_T, best_params_T = cost_estimator(
+    best_cost_t, best_params_t = cost_estimator(
         num_logical_qubits=num_logical_qubits,
-        num_T=num_T,
-        surface_code_cycle_time=SCC_time,
+        num_t=num_t,
+        surface_code_cycle_time=scc_time,
         physical_error_rate=physical_error_rate,
     )
-    numpy.testing.assert_almost_equal(
-        2
-        * (
-            best_cost_toffoli.algorithm_failure_probability
-            - best_cost_T.algorithm_failure_probability
-        )
-        / num_toffoli,
-        best_params_toffoli.magic_state_factory.failure_rate,
-    )
-    """
-    X+Yf - (X+Yf/2) = Yf/2, where X+Yf and X+Yf/2 are
-    Algorithm Failure Prob for Toffoli and T cases respectively,
-    where X -> data failure, Yf -> distillation failure,
-      Y=#Toffoli gate=#T gates and f is 1 CCZ error.
-    If failure rate is calculated correctly, to ascertain
-    if Algorithm Failure Probability is calculated correctly
-    for both toffoli and T.
-    """
-
     numpy.testing.assert_almost_equal(
         (
             best_cost_toffoli.algorithm_failure_probability
-            - best_cost_T.algorithm_failure_probability
-        )
-        / num_T,
-        best_params_T.magic_state_factory.failure_rate,
+            - 2 * best_cost_t.algorithm_failure_probability
+        ),
+        0,
+    )
+
+
+def test_algorithm_failure_prob_calculation():
+    """
+    To ascertain if algorithm failure probability is
+    calculated correctly for a ckt with (num_toffoli=20,
+    num_t=20) and for a ckt with (num_toffoli=30,
+    num_t=0)
+    """
+    num_logical_qubits = 12
+    scc_time = 0.000002
+    physical_error_rate = 1.0e-4
+    best_cost_toffoli, best_params_toffoli = cost_estimator(
+        num_logical_qubits=num_logical_qubits,
+        num_toffoli=20,
+        num_t=20,
+        surface_code_cycle_time=scc_time,
+        physical_error_rate=physical_error_rate,
+    )
+    best_cost_t, best_params_t = cost_estimator(
+        num_logical_qubits=num_logical_qubits,
+        num_toffoli=30,
+        num_t=0,
+        surface_code_cycle_time=scc_time,
+        physical_error_rate=physical_error_rate,
+    )
+    numpy.testing.assert_almost_equal(
+        best_cost_toffoli.algorithm_failure_probability,
+        best_cost_t.algorithm_failure_probability,
     )
 
 
 def test_default_T_factories():
     num_logical_qubits = 12
-    SCC_time = 0.000002
+    scc_time = 0.000002
     physical_error_rate = 1.0e-3
     # Default Magic State Factory details used based
     # on this physical error rate.
-    num_T = 200
+    num_t = 200
     num_toffoli = 140
-    best_cost_T, best_params_T = cost_estimator(
+    best_cost_t, best_params_t = cost_estimator(
         num_logical_qubits=num_logical_qubits,
-        num_T=num_T,
-        surface_code_cycle_time=SCC_time,
+        num_t=num_t,
+        surface_code_cycle_time=scc_time,
         physical_error_rate=physical_error_rate,
     )
     best_cost, best_params_toffoli = cost_estimator(
         num_logical_qubits=num_logical_qubits,
         num_toffoli=num_toffoli,
-        surface_code_cycle_time=SCC_time,
+        surface_code_cycle_time=scc_time,
         physical_error_rate=physical_error_rate,
     )
-    assert (
-        best_params_T.magic_state_factory.rounds == 186
-        and best_params_T.magic_state_factory.failure_rate == 3.6000000000000003e-16
+    numpy.testing.assert_allclose(best_params_t.magic_state_factory.rounds, 186)
+    numpy.testing.assert_allclose(
+        best_params_t.magic_state_factory.failure_rate, 3.6e-16
     )
-    assert (
-        best_params_toffoli.magic_state_factory.rounds == 186
-        and best_params_toffoli.magic_state_factory.failure_rate
-        == 3.6000000000000003e-16
+    numpy.testing.assert_allclose(best_params_toffoli.magic_state_factory.rounds, 186)
+    numpy.testing.assert_allclose(
+        best_params_toffoli.magic_state_factory.failure_rate, 3.6e-16
     )
 
 
 def test_default_values():
-    num_logical_qubits = 12
-    physical_error_rate = 1.0e-4
     """
     If physical_error_rate != default value,
-    this ascertains if both num_T=0 and num_toffoli=0,
-    then it must throw an error.
+    this ascertains if both num_t=0 and num_toffoli=0,
+    then it must throw an error. This basically tests
+    the 'else statement' in iter_known_factories()
     """
-    with pytest.raises(SystemExit) as dvalue:
+    num_logical_qubits = 12
+    physical_error_rate = 1.0e-4
+    with pytest.raises(ValueError) as dvalue:
         a, b = cost_estimator(
             num_logical_qubits=num_logical_qubits,
             physical_error_rate=physical_error_rate,
         )
 
-    assert dvalue.type == SystemExit
-    assert dvalue.value.code == 1
+    assert dvalue.type == ValueError
 
 
 def test_all_default_values():
-    num_logical_qubits = 12
     """
     If physical_error_rate == default value,
-    this ascertains if both num_T=0 and num_toffoli=0,
-    then it must throw an error.
+     this ascertains if both num_t=0 and num_toffoli=0,
+     then it must throw an error. This basically tests
+     the 'if statement' in iter_known_factories()
     """
-    with pytest.raises(SystemExit) as dvalue:
+    num_logical_qubits = 12
+    with pytest.raises(ValueError) as dvalue:
         a, b = cost_estimator(
             num_logical_qubits=num_logical_qubits,
         )
 
-    assert dvalue.type == SystemExit
-    assert dvalue.value.code == 1
+    assert dvalue.type == ValueError
