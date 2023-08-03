5f578bfe3c2a3372d3f0a880ec91ffcc085bfb6c
Sebastian Morawiec <Sebastian.Morawiec@zapatacomputing.com>
2023-08-03 16:16:23 +0200

    style fixes

--- src/benchq/resource_estimation/graph/extrapolation_estimator.py
+++ src/benchq/resource_estimation/graph/extrapolation_estimator.py
@@ -91,9 +91,12 @@ class ExtrapolationResourceEstimator(GraphResourceEstimator):
         resource_info = self.estimate_resources_from_graph_data(
             extrapolated_info, algorithm_description
         )
+        extrapolated_extra = extrapolated_info.return_as_resource_estimate(
+            resource_info.extra.graph_measure_ratio
+        )
         info = ExtrapolatedGraphResourceInfo(
             n_logical_qubits=resource_info.n_logical_qubits,
-            extra=replace(extrapolated_info, n_nodes=resource_info.extra.n_nodes),
+            extra=replace(extrapolated_extra, n_nodes=resource_info.extra.n_nodes),
             code_distance=resource_info.code_distance,
             logical_error_rate=resource_info.logical_error_rate,
             total_time_in_seconds=resource_info.total_time_in_seconds,
