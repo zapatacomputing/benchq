f08b17cdfdda43cd8c929cab24b5d085e040716c
Bhargav <bhargav19036@gmail.com>
2023-08-04 16:17:47 -0400

    Methods for ADD^L and ADD_dagger

---
+++ src/benchq/algorithms/utils/compression_gadget.py
@@ -0,0 +1,20 @@
+from orquestra.quantum.circuits import Circuit
+import math
+from benchq.block_encodings.block_encoding_utils import controlled_clock
+
+
+
+def get_add_l(L) -> Circuit:
+    add_l_circuit = Circuit()
+    for _ in range(L):
+        size = math.ceil(math.log2(L)) + 1
+        add_l_circuit += controlled_clock(size)
+
+    return add_l_circuit
+
+
+def get_add_dagger(L) -> Circuit:
+    add_dagger_circuit = Circuit()
+    size = math.ceil(math.log2(L)) + 1
+    add_dagger_circuit = controlled_clock(size, direction="backward")
+    return add_dagger_circuit
