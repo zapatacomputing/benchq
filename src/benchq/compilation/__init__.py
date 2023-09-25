b096e8f9b9f8df4361fb35df0c2928f589b91c78
Athena Caesura <mathmeetsmusic@gmail.com>
2023-09-25 14:52:29 -0400

    fix: get rid of juliapkg

--- src/benchq/compilation/__init__.py
+++ src/benchq/compilation/__init__.py
@@ -4,7 +4,10 @@
 import os
 import pathlib
 
+print("made it here!")
 from juliacall import Main as jl
+
+print("now here!")
 
 from .julia_utils import (
     get_algorithmic_graph_and_icm_output,
@@ -15,9 +18,17 @@ from .julia_utils import (
 from .pyliqtr_transpilation import pyliqtr_transpile_to_clifford_t
 from .transpile_to_native_gates import transpile_to_native_gates
 
+import juliapkg
+
+juliapkg.require_julia("1.9.3")
+juliapkg.add("JSON", "682c06a0-de6a-54ab-a142-c8b1cf79cde6", version="0.21")
+juliapkg.add("Jabalizer", "5ba14d91-d028-496b-b148-c0fbc366f709", version="0.4.4")
+juliapkg.add("TimerOutputs", "a759f4b9-e2f1-59dc-863e-4aeb61b1ea8f", version="0.5.23")
+juliapkg.add("StatsBase", "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91", version="0.34.0")
+
+juliapkg.resolve()
+
 jl.include(
     os.path.join(pathlib.Path(__file__).parent.resolve(), "jabalizer_wrapper.jl"),
 )
-
-
 jl.include(os.path.join(pathlib.Path(__file__).parent.resolve(), "ruby_slippers.jl"))
---
+++ src/benchq/compilation/install_deps.jl
@@ -0,0 +1,6 @@
+using Pkg
+
+Pkg.add(name="JSON", version="0.21")
+Pkg.add(name="Jabalizer", version="0.4.4")
+Pkg.add(name="TimerOutputs", version="0.5.23")
+Pkg.add(name="StatsBase", version="0.34.0")
