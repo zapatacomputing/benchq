1ad18ed12695035b4109e324a0d4219a831e02c0
Athena Caesura <mathmeetsmusic@gmail.com>
2023-09-26 16:37:31 -0400

    fix: don't install julia twice

--- src/benchq/compilation/__init__.py
+++ src/benchq/compilation/__init__.py
@@ -4,11 +4,6 @@
 import os
 import pathlib
 
-print("made it here!")
-from juliacall import Main as jl
-
-print("now here!")
-
 from .julia_utils import (
     get_algorithmic_graph_and_icm_output,
     get_algorithmic_graph_from_Jabalizer,
@@ -20,6 +15,8 @@ from .transpile_to_native_gates import transpile_to_native_gates
 
 import juliapkg
 
+# juliapkg.json is undesirable because you have to put it in the same directory as the
+# script that calls it. So we have to do this hacky thing to get dependencies.
 juliapkg.require_julia("1.9.3")
 juliapkg.add("JSON", "682c06a0-de6a-54ab-a142-c8b1cf79cde6", version="0.21")
 juliapkg.add("Jabalizer", "5ba14d91-d028-496b-b148-c0fbc366f709", version="0.4.4")
@@ -27,6 +24,9 @@ juliapkg.add("TimerOutputs", "a759f4b9-e2f1-59dc-863e-4aeb61b1ea8f", version="0.
 juliapkg.add("StatsBase", "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91", version="0.34.0")
 
 juliapkg.resolve()
+
+
+from juliacall import Main as jl  # noqa: E402
 
 jl.include(
     os.path.join(pathlib.Path(__file__).parent.resolve(), "jabalizer_wrapper.jl"),
