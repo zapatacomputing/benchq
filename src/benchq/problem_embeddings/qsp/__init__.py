defe2069c0a36f94f70a45bb17e3ec8317860ea9
Peter Johnson <peter.d.johnson22@gmail.com>
2024-08-03 21:28:42 -0400

    Fixing init

--- src/benchq/problem_embeddings/qsp/__init__.py
+++ src/benchq/problem_embeddings/qsp/__init__.py
@@ -2,4 +2,4 @@
 # © Copyright 2022-2023 Zapata Computing Inc.
 ################################################################################
 from ._lin_and_dong_qsp import get_lin_and_dong_qsp_circuit
-from ._qsp import get_qsp_circuit, get_qsp_program
+from ._qsp import get_qsp_program_from_block_encoding
