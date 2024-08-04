fbc6e9ba24ff464a3672709fea49f113f0ed7ae7
Peter Johnson <peter.d.johnson22@gmail.com>
2024-08-03 21:24:49 -0400

    Updating example to take in qualtran block encoding

--- examples/ex_2_time_evolution.py
+++ examples/ex_2_time_evolution.py
@@ -18,7 +18,20 @@ main to install the extras. Then run `orq start` to start local ray.
 from pprint import pprint
 from time import time
 
-from benchq.algorithms.time_evolution import qsp_time_evolution_algorithm
+
+from pyLIQTR.BlockEncodings.getEncoding import getEncoding, VALID_ENCODINGS
+from pyLIQTR.clam.lattice_definitions import (
+    CubicLattice,
+    SquareLattice,
+    TriangularLattice,
+)
+from pyLIQTR.ProblemInstances.getInstance import getInstance
+
+
+from benchq.algorithms.time_evolution import (
+    qsp_time_evolution_algorithm_from_block_encoding,
+)
+
 
 # Requires jabalizer install. See README.
 # from benchq.compilation.graph_states import get_jabalizer_circuit_compiler
@@ -26,33 +39,40 @@ from benchq.compilation.graph_states import get_ruby_slippers_circuit_compiler
 from benchq.compilation.graph_states.implementation_compiler import (
     get_implementation_compiler,
 )
-from benchq.problem_ingestion import generate_fermi_hubbard_jw_qubit_hamiltonian
+
 from benchq.quantum_hardware_modeling import BASIC_SC_ARCHITECTURE_MODEL
 from benchq.resource_estimators.graph_estimator import GraphResourceEstimator
 from benchq.timing import measure_time
 
 
 def main():
-    evolution_time = 5
-
-    start_time = time()
-    # Generating Hamiltonian for a given set of parameters, which
-    # defines the problem we try to solve.
-    # measure_time is a utility tool which measures the execution time of
-    # the code inside the with statement.
-    with measure_time() as t_info:
-        N = 2  # Problem size
-
-        # Get a Fermi-Hubbard Hamiltonian for simulation
-        operator = generate_fermi_hubbard_jw_qubit_hamiltonian(N, N, 1.0, -2.0)
-
-    print("Operator generation time:", t_info.total)
 
     # Here we generate the AlgorithmImplementation structure, which contains
     # information such as what subroutine needs to be executed and how many times.
     # In this example we perform time evolution using the QSP algorithm.
+
+    start_time = time()
+
+    # Fermi-Hubbard model parameters
+    J = -1.0
+    N = 2
+    U = 4.0
+    shape = (N, N)
+
+    # Generate the Fermi-Hubbard model instance
+    model = getInstance("FermiHubbard", shape=shape, J=J, U=U, cell=SquareLattice)
+
+    # Get the block encoding for the Fermi-Hubbard model
+    encoding = getEncoding(VALID_ENCODINGS.FermiHubbardSquare)
+    block_encoding = encoding(model)
+
+    evolution_time = 5.0 * block_encoding.alpha
+    failure_tolerance = 1e-4
+
     with measure_time() as t_info:
-        algorithm = qsp_time_evolution_algorithm(operator, evolution_time, 1e-3)
+        algorithm = qsp_time_evolution_algorithm_from_block_encoding(
+            block_encoding, evolution_time, failure_tolerance
+        )
 
     print("Circuit generation time:", t_info.total)
     print("n qubits in circuit:", algorithm.program.subroutines[0].n_qubits)
